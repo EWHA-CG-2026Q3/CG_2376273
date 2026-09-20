a54ca8d4b3a6630c6f374e21dd4efb7c6f56bcf1
sangeuncho <theancestoris@naver.com>
2026-09-20 11:38:27 +0900

    S03 폴리곤 템플릿 편집

---
+++ Assets/S03/S03_Mesh2D/S03_CustomPolygonMesh_Square.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+[ExecuteAlways]
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class S03_CustomPolygonMesh_Square : MonoBehaviour
+{
+    void Start()
+    {
+        // TODO 1: 정점 5개 좌표 작성 (오각형 형태)
+        Vector3[] vertices = new Vector3[]
+        {
+            new Vector3(0f, 1.5f, 0f),    // 0번: 맨 위 꼭짓점
+            new Vector3(1.5f, 0.5f, 0f),  // 1번: 오른쪽 위
+            new Vector3(1f, -1f, 0f),     // 2번: 오른쪽 아래
+            new Vector3(-1f, -1f, 0f),    // 3번: 왼쪽 아래
+            new Vector3(-1.5f, 0.5f, 0f)  // 4번: 왼쪽 위
+        };
+
+        // TODO 2: 시계 방향으로 삼각형 3개 연결 (총 9개 인덱스)
+        int[] triangles = new int[]
+        {
+            0, 1, 4, // 첫 번째 삼각형 (위쪽)
+            1, 2, 4, // 두 번째 삼각형 (중앙)
+            2, 3, 4  // 세 번째 삼각형 (아래쪽)
+        };
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+
+        GetComponent<MeshFilter>().mesh = mesh;
+        GetComponent<MeshRenderer>().sharedMaterial = new Material(Shader.Find("Universal Render Pipeline/Lit"));
+    }
+}
